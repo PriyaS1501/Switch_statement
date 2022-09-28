d3ae7ce7ba4d34e8e4abbfa0320dc5d78170eb02
PriyaS1501 <babarpriyanka323@gmail.com>
2022-09-28 14:54:11 +0530

    currency code

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let c_code = "EUR";
+
+switch (c_code) {
+    case "EUR": console.log("Euro");
+        break;
+    case "INR": console.log("Indian Rupee");
+        break;
+    case "AUD": console.log("Australian Dollar");
+        break;
+    case "IDR": console.log("Rupiah");
+        break;
+    case "NPR": console.log("Nepalese Rupee");
+        break;
+    case "USD": console.log("US Dollar");
+        break;
+    case "PKR": console.log("Pakistan Rupee");
+        break;
+    default :console.log("Sorry,  Currency code " + c_code + " is not exist.");
+}
+
+
+
